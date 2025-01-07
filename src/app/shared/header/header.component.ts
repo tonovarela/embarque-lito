eaf2534e3232df3c053432b35cf9a25fb452b80e
tonovarela <tonovarela@live.com>
2025-01-07 17:22:10 -0600

    Agregar soporte para temas claros y oscuros en el componente Header. Refactorizar la carga de estilos de Syncfusion y optimizar la gestión de temas en la aplicación.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -12,26 +12,41 @@ import { UiService } from '@app/services/ui.service';
 export class HeaderComponent  implements OnInit {
    esDark = false;
    uiService = inject(UiService);
+
+   linksMap: Map<string, HTMLLinkElement> = new Map();
+
   ngOnInit(): void {
     
-    
+    const links = Array.from(document.querySelectorAll('link')) as HTMLLinkElement[]; 
+    const _links = links.filter(link => link.getAttribute('rel') === 'stylesheet' && link.getAttribute('href')?.includes('syncfusion') ).map(link => {link.setAttribute('disabled', 'true');return link;});    
+    _links.forEach(link => {      
+         this.linksMap.set(link.getAttribute('href')?.includes('dark')?"dark":'light', link);         
+    });
+        
     if(!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches){
       localStorage.setItem('color-theme', 'light');
+      this.linksMap.get('light')?.removeAttribute('disabled');  
+      console.log('prefers-color-scheme: dark');    
     }
 
     if (localStorage.getItem('color-theme') === 'dark' ) {
-      document.documentElement.classList.add('dark');            
+      document.documentElement.classList.add('dark');         
+      this.linksMap.get('dark')?.removeAttribute('disabled');         
       this.esDark = true;
     }
+    console.log(this.linksMap.get('dark'));
+    console.log(this.linksMap.get('light'));
 
   }
   cambiarTema() {    
     if (localStorage.getItem('color-theme') === 'dark' ) {
       document.documentElement.classList.remove('dark');
+      
       localStorage.setItem('color-theme', 'light');
       this.esDark = false;
     } else {
       document.documentElement.classList.add('dark');
+      
       localStorage.setItem('color-theme', 'dark');
       this.esDark = true;
     }
