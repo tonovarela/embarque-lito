cb00d7536f6e2c97ddb901c8f4bca97ad7d41dec
tonovarela <tonovarela@live.com>
2025-05-06 10:54:14 -0600

    Agregar lógica para determinar el estilo de mapa según el tema oscuro y mejorar la visualización de la firma en el componente de listado de recorridos.

--- src/app/helpers/getImagePosition.ts
+++ src/app/helpers/getImagePosition.ts
@@ -1,6 +1,11 @@
 import { environment } from "@environments/environment.development";
 
 
+const isDark =():boolean =>{
+  return document.documentElement.classList.contains('dark')
+    
+}
+
 export const  getMapboxImageUrl =(
     lng: number,
     lat: number,
@@ -8,7 +13,10 @@ export const  getMapboxImageUrl =(
     width: number=600,
     height: number =400
   ): string=> {
+
+    
+    
     // Reemplaza con tu estilo de mapa preferido
-    const mapStyle = 'mapbox/streets-v11';
+    const mapStyle = `mapbox/${isDark()?'dark-v11':'streets-v12'}`;
     return `https://api.mapbox.com/styles/v1/${mapStyle}/static/${lng},${lat},${zoom}/${width}x${height}?access_token=${environment.mapboxToken}`;
   }
