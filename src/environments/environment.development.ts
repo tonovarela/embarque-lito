2f40f56617e0d5874ddd7bcccd8ce4d043876ca6
tonovarela <tonovarela@live.com>
2025-08-25 16:54:49 -0600

    Cambios

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,9 +2,11 @@ export const environment = {
     production:false,
     API_URL:'https://servicios.litoprocess.com/embarques',
    // userDev: {username:'sserrano', password: '54622'},
-  //  userDev: {username:'jahernandez', password: '54625'},
-     //userDev: {username:'jamora', password: '54624'},
-    //  userDev: {username:'flopez', password: '54623'},
+   //userDev: {username:'jahernandez', password: '54625'},
+   //  userDev: {username:'jamora', password: '54624'},
+     //userDev: {username:'flopez', password: '54623'},
     userDev: {username:'mestelles', password: '54321'},
+    //userDev: {username:'jmiranda', password: '54621'},
+    
     mapboxToken:"pk.eyJ1IjoidG9ub3ZhcmVsYSIsImEiOiJjazdhdTlqczUxN3VnM2VwNTFuZTV0cWp6In0.454u4lgDsq-FdCfpv5AmnQ"
 };
