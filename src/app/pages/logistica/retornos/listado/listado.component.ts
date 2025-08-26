1d959c65a9f3685caff0164d0c2c658163eeb46b
tonovarela <tonovarela@live.com>
2025-08-26 14:28:55 -0600

    Agregar componente de formulario para nuevos retornos y actualizar el botón flotante para enlazar a la nueva ruta.

--- src/app/pages/logistica/retornos/listado/listado.component.ts
+++ src/app/pages/logistica/retornos/listado/listado.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FabbuttonComponent } from '@app/shared/fabbutton/fabbutton.component';
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [],
+  imports: [FabbuttonComponent],
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/logistica/retornos/nueva/nueva.component.ts
+++ src/app/pages/logistica/retornos/nueva/nueva.component.ts
@@ -1,11 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PrimeNgModule } from '@app/lib/primeng.module';
+import { AutocompleteComponent } from '@app/shared/autocomplete/autocomplete.component';
 
 @Component({
   selector: 'app-nueva',
   standalone: true,
-  imports: [],
+  imports: [AutocompleteComponent,PrimeNgModule, ReactiveFormsModule,],
   templateUrl: './nueva.component.html',
   styleUrl: './nueva.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export  default class NuevaComponent { }
+export  default class NuevaComponent {
+
+
+  onSelectOP(event: any) {
+    console.log(event);   
+
+ }
+
+}
